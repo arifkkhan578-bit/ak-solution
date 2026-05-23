5a2bfef7c116d24f847c9d7fc77448fc33d524db
ARIF KHAN <arifkhan@ARIFs-MacBook-Air.local>
2026-05-24 04:04:46 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // MongoDB Connection
